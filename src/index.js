cbf367fb973e48d5b8df11916e9328ed6246606d
zen-yang <519796642@qq.com>
2020-01-16 23:37:36 +0800

    准备上传

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import x from './x.js'
-console.log(x)
-console.log('hi2')
+import png from './assets/1.png'
+console.log(png)
+
+const div = document.getElementById('app')
+
+div.innerHTML = `
+  <img src="${png}">
+`
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick = () => {
+  const promise = import('./lazy')
+  promise.then((module)=>{
+    const fn = module.default
+    fn()
+  }, ()=>{
+    console.log('模块加载错误')
+  })
+}
+
+div.appendChild(button)
